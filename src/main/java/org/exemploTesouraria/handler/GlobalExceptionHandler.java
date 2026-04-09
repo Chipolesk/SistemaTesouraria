d82b83623caec9efae53e614e22ca9bdf823603e
Chipolesk <danielchipolesk@gmail.com>
2026-04-09 01:15:12 -0300

    fix error in GlobalExeptionHandler: add any header
    Signed-off-by: Chipolesk <danielchipolesk@gmail.com>

--- src/main/java/org/exemploTesouraria/handler/GlobalExceptionHandler.java
+++ src/main/java/org/exemploTesouraria/handler/GlobalExceptionHandler.java
@@ -1,17 +1,23 @@
 package org.exemploTesouraria.handler;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
+import org.exemploTesouraria.DTO.ApiError;
 import org.exemploTesouraria.exception.DataConflictException;
 import org.exemploTesouraria.exception.ResourceNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
